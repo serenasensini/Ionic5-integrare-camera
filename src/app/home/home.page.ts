ae2eb9bd1f1aa98f1da46c94dd2ddfe8ad1779d3
Serena Sensini <serena.sensini@gmail.com>
2020-01-13 19:56:27 +0100

    options di Camera

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,41 +6,31 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-})
+});
 export class HomePage {
 
   foto: any = '';
-
-  options: CameraOptions = {
-    quality: 100,
-    destinationType: this.camera.DestinationType.FILE_URI,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
-  };
 
   constructor(private camera: Camera) { }
 
   scattaFoto(): void {
 
-    // Create options for the Camera Dialog
     const options = {
       quality: 100,
-      targetWidth: 80,
-      targetHeight: 80,
       destinationType: this.camera.DestinationType.DATA_URL,
       sourceType: this.camera.PictureSourceType.CAMERA,
-      correctOrientation: true
+      encodingType: this.camera.EncodingType.JPEG,
+      targetWidth: 150,
+      targetHeight: 150,
+      saveToPhotoAlbum: true
     };
 
-// Get the data of an image
     this.camera.getPicture(options).then((imagePath) => {
-      this.prepareFile('data:image/jpeg;base64,' + imagePath);
+      this.foto = 'data:image/jpeg;base64,' + imagePath;
     }, (err) => {
-      console.log('Error while selecting image.');
-    })};
-
-  private prepareFile(namePath) {
-    this.foto = namePath;
+      console.log('Errore durante il caricamento dell\'immagine.');
+    });
   }
+
 
 }
